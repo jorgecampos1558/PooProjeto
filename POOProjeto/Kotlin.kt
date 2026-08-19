b0b0f2951b70861ebed260f15d515010cf7a2323
JORGE CAMPOS <38052380+jorgecampos1558@users.noreply.github.com>
2026-08-19 02:55:15 -0300

    Delete POOProjeto directory

--- POOProjeto/Kotlin.kt
+++
@@ -1,8 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Kotlin.kt to edit this template
- */
-
-fun main(args: Array<String>) {
-  println("Hello World!")
-}
